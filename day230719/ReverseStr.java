22ac757721fbe19132e9877fdd38e6650f0c8562
Administrator <hjh970422@gmail.com>
2023-07-19 10:32:15 +0900

    230719

---
+++ day230719/ReverseStr.java
@@ -0,0 +1,27 @@
+package day230719;
+
+import java.util.Scanner;
+
+public class ReverseStr
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("input : ");
+		String str = sc.nextLine();
+		String result = "";
+		
+		System.out.print("output : ");
+		for(int i = str.length() - 1; i >= 0; i--)
+		{
+			result += str.charAt(i);
+		}
+		
+		System.out.println(result);
+		
+//		System.out.print("output : ");
+//		for(int i = str.length() - 1; i >= 0; i--)
+//			System.out.print(str.charAt(i));
+	}
+}
