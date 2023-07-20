2166779c2b8cee20655d2e806c43308a12ea8394
hjh <hjh970422@gmail.com>
2023-07-20 16:17:42 +0900

    230720

---
+++ day230720/Week.java
@@ -0,0 +1,12 @@
+package day230720;
+
+public enum Week
+{
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+}
---
+++ day230720/WeekExample2.java
@@ -0,0 +1,38 @@
+package day230720;
+
+import java.util.Calendar;
+
+public class WeekExample2
+{
+	public static void main(String[] args)
+	{
+		Week today = null;
+		
+		Calendar cal = Calendar.getInstance();
+
+		int week = cal.get(Calendar.DAY_OF_WEEK);
+		
+		today = switch (week)
+				{
+					case 1 -> Week.SUNDAY;
+					case 2 -> Week.MONDAY;
+					case 3 -> Week.TUESDAY;
+					case 4 -> Week.WEDNESDAY;
+					case 5 -> Week.THURSDAY;
+					case 6 -> Week.FRIDAY;
+					case 7 -> Week.SATURDAY;
+					default -> null;
+				};
+		
+		System.out.print("오늘은 ");
+		// 1
+		if((Week.valueOf(today.toString()) == Week.SUNDAY) || (Week.valueOf(today.toString()) == Week.SATURDAY))
+				System.out.println("주말");
+		else
+			System.out.println("평일");
+		
+		// 2
+		for(Week day : Week.values())
+			System.out.print(day + " ");
+	}
+}
