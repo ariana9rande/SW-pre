d538188a40be33dfde19152bb4de18875e6e8dc7
hjh <hjh970422@gmail.com>
2023-07-24 10:55:48 +0900

    230724

---
+++ day230724/Movie.java
@@ -0,0 +1,17 @@
+package day230724;
+
+public class Movie
+{
+	String name, director;
+	
+	public Movie()
+	{
+		
+	}
+	
+	public Movie(String name, String director)
+	{
+		this.name = name;
+		this.director = director;
+	}
+}
---
+++ day230724/MovieExample.java
@@ -0,0 +1,29 @@
+package day230724;
+
+import java.util.Scanner;
+
+public class MovieExample
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		Movie[] arr = new Movie[2];
+		
+		for(int i = 0; i < arr.length; i++)
+		{
+			arr[i] = new Movie();
+			
+			System.out.print("name : ");
+			arr[i].name = sc.nextLine();
+			
+			System.out.print("director : ");
+			arr[i].director = sc.nextLine();
+		}
+		
+		for(int i = 0; i < arr.length; i++)
+		{
+			System.out.println(arr[i].name + ", " + arr[i].director);
+		}
+	}
+}
